e4896b87641c0d55fabf5149b4674a3d058017b3
Bashar_Modallal <bashar.mod497@gmail.com>
2020-09-14 02:04:28 +0300

    Add Demo Page For Testing

--- file_manager/routes/web.php
+++ file_manager/routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
